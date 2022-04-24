ca2087f445c680958fd8791ce431ee25bd0462f1
mike <gnosoir@hotmail.com>
2022-04-24 16:28:20 +0800

    new API tester

--- tests/spa_tester.py
+++ tests/spa_tester.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sun_earth_geometry import SunPositionAnalysis
+import sun_earth_geometry
 
 # spd = sp.SPData()
 
@@ -23,30 +23,35 @@ from sun_earth_geometry import SunPositionAnalysis
 # sp.spa_calculate(spd, True)
 
 if __name__ == "__main__":
-    # spa = SunPositionAnalysis(year=2003, month=10, day=17,
-    #     hour=12, minute=30, second=30,
-    #     timezone=-7.0, delta_ut1=0, delta_t=67,
-    #     longitude=-105.1786, latitude=39.742476, elevation=1830.14,
-    #     pressure=820, temperature=11, atmos_refract=0.5667)
-    input2 = {
-        'timezone':-7.0, 'delta_ut1':0, 'delta_t':67,
-        'longitude':-105.1786, 'latitude':39.742476, 'elevation':1830.14,
-        'pressure':820, 'temperature':11, 'atmos_refract':0.5667
-    }
-    input_full = {
-        'year':2003, 'month':10, 'day':17,
-        'hour':12, 'minute':30, 'second':30, **input2
+    if sun_earth_geometry.__version__ >= '0.3':
+        print("Deprecated API tester")
+    else:
+        from sun_earth_geometry import SunPositionAnalysis
+
+        # spa = SunPositionAnalysis(year=2003, month=10, day=17,
+        #     hour=12, minute=30, second=30,
+        #     timezone=-7.0, delta_ut1=0, delta_t=67,
+        #     longitude=-105.1786, latitude=39.742476, elevation=1830.14,
+        #     pressure=820, temperature=11, atmos_refract=0.5667)
+        input2 = {
+            'timezone':-7.0, 'delta_ut1':0, 'delta_t':67,
+            'longitude':-105.1786, 'latitude':39.742476, 'elevation':1830.14,
+            'pressure':820, 'temperature':11, 'atmos_refract':0.5667
         }
-    # spa = SunPositionAnalysis(**input_full)
-    # spa = SunPositionAnalysis(year=2003, month=10, day=17, **input2)
-    spa = SunPositionAnalysis("2003-10-17 12:30:30", **input2)
-    # spa = SunPositionAnalysis(
-    #     datetime.strptime("2003-10-17 12:30:30", "%Y-%m-%d %H:%M:%S"),
-    #     **input2)
+        input_full = {
+            'year':2003, 'month':10, 'day':17,
+            'hour':12, 'minute':30, 'second':30, **input2
+            }
+        # spa = SunPositionAnalysis(**input_full)
+        # spa = SunPositionAnalysis(year=2003, month=10, day=17, **input2)
+        spa = SunPositionAnalysis("2003-10-17 12:30:30", **input2)
+        # spa = SunPositionAnalysis(
+        #     datetime.strptime("2003-10-17 12:30:30", "%Y-%m-%d %H:%M:%S"),
+        #     **input2)
     
-    print("----------INPUT----------")
-    spa.print_input()
-    spa() # spa(with_sun_rts=False)
-    # spa(with_sun_rts=True)
-    print("----------OUTPUT----------")
-    spa.print_output()
+        print("----------INPUT----------")
+        spa.print_input()
+        spa() # spa(with_sun_rts=False)
+        # spa(with_sun_rts=True)
+        print("----------OUTPUT----------")
+        spa.print_output()
