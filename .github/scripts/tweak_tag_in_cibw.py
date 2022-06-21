82624b2c944cb5f143e1993136654779e7a6e209
mike <gnosoir@hotmail.com>
2022-06-22 00:08:32 +0800

    fix bug in tweak_tag_in_cibw.py

--- .github/scripts/tweak_tag_in_cibw.py
+++ .github/scripts/tweak_tag_in_cibw.py
@@ -3,21 +3,25 @@
 import subprocess
 from pathlib import Path
 
-import versioningit
+from versioningit import Versioningit
 
 
 def main():
     project_dir = Path(__file__).parent.parent.parent
 
-    rev = versioningit.get_version(project_dir)
-    next_rev = versioningit.get_next_version(project_dir)
-
-    if rev == next_rev:
-        print(f"No tag tweaking needed: v{rev}")
+    vgit = Versioningit.from_project_dir(project_dir)
+    report = vgit.run(write=False, fallback=True)
+    if report.description.state == "exact":
+        print(f"No tag tweaking needed: {report.base_version}")
         return
-
-    print(f"Tweaking tag for CI: {rev} -> {next_rev}")
-    subprocess.check_call(["git", "tag", f"v{next_rev}", "HEAD"], cwd=project_dir)
+    else:
+        print(
+            "VCS state is %r; Tweaking tag for CI: %r -> %r"
+            % (report.description.state, report.base_version, report.next_version)
+        )
+        subprocess.check_call(
+            ["git", "tag", f"v{report.next_version}", "HEAD"], cwd=project_dir
+        )
 
 
 if __name__ == "__main__":
