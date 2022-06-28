b39342e7fa4fcedca3183325c3dbb67b9b512b19
Mike Song <mikesongming@gmail.com>
2022-06-29 01:42:26 +0800

    use toml recording case data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
-from datetime import datetime
+from pathlib import Path
 
 import pytest
+import tomli
 
 from fseg import SunEarthAnalyzer
 
-from .case_data import sun_position_data
+TEST_ROOT = Path(__file__).parent
+with open(TEST_ROOT.joinpath("case_data.toml"), "rb") as f:
+    case_data = tomli.load(f)["case_data"]
 
 
 @pytest.fixture(scope="module", params=["SPA"])
@@ -13,20 +16,12 @@ def sun_earth_analyzer(request):
     yield sea
 
 
-@pytest.fixture(scope="module", params=range(len(sun_position_data)))
+@pytest.fixture(scope="module", params=case_data["sun_position_at"])
 def sun_position_case(request):
-    case_id = request.param
-    case_data = sun_position_data[case_id]
-    case_input = case_data["input"]
-    case_output = case_data["output"]
-    obs = case_input["observatory"]
-    obs_time_t = tuple(
-        case_input["time"].get(k)
-        for k in ["year", "month", "day", "hour", "minute", "second"]
-    )
-    sp_result = case_output
+    input = request.param["input"]
+    output = request.param["output"]
 
-    yield (obs, obs_time_t, sp_result)
+    yield (input["observatory"], input["time"], output)
 
 
 @pytest.fixture(scope="module")
@@ -35,18 +30,25 @@ def observatory(sun_position_case):
 
 
 @pytest.fixture(scope="module")
+def obs_time_datetime(sun_position_case):
+    return sun_position_case[1]
+
+
+@pytest.fixture(scope="module")
 def obs_time_str(obs_time_datetime):
     return obs_time_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
 
 @pytest.fixture(scope="module")
-def obs_time_datetime(obs_time_t):
-    return datetime(*obs_time_t)
-
-
-@pytest.fixture(scope="module")
-def obs_time_t(sun_position_case):
-    return sun_position_case[1]
+def obs_time_t(obs_time_datetime):
+    return (
+        obs_time_datetime.year,
+        obs_time_datetime.month,
+        obs_time_datetime.day,
+        obs_time_datetime.hour,
+        obs_time_datetime.minute,
+        obs_time_datetime.second,
+    )
 
 
 @pytest.fixture(scope="module")
