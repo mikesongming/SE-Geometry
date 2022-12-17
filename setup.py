82355727469d4b4123d99c7f651310a07ddb028e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-17 01:50:30 +0000

    Adding tarfile member sanitization to extractall()

--- setup.py
+++ setup.py
@@ -21,7 +21,26 @@ def download_and_extract_tzdata2022a(tempdir: Path):
     local_tzdata_dir = tempdir.joinpath(Path(Path(local_iana_file.name).stem).stem)
 
     with tarfile.open(local_iana_file, "r:gz") as tar:
-        tar.extractall(local_tzdata_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, local_tzdata_dir)
 
     return local_tzdata_dir
 
